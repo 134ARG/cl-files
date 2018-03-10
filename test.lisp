26ceff8e37a74a39c952c1dcf2646118df63c41a
134ARG <zryzry@outlook.com>
2018-03-11 02:16:46 +0800

    works, infix to prefix hasnt finished.

--- test.lisp
+++ test.lisp
@@ -1,23 +1,52 @@
-(defun most (fn lst)
-  (if (null lst) (values 0 nil))
-  (let* ((wins (car lst))
-	 (max (funcall fn wins)))
-    (dolist (x (cdr lst))                     ;you needn't. just reserve the car and cdr.
-      (let ((score (funcall fn x)))
-	(if (> score max)
-	    (setf wins x
-		  max score))))
-    (values wins max)))
-
-(defun before (x y lst &key (test #'equal))
-  (let ((first (car lst)))
-    (and lst
-	 (cond ((funcall test x first) lst)      ;you'd better choose to return sth useful.
-	       ((funcall test y first) nil)
-	       (t (before x y (cdr lst)))))))
-
-(defun after (x y lst &key (test #'equal))       ;you should be aware that you just need to check the returned value by before
-  (let (rest (before y x lst :test test))
-    (and (not rest) (member x lst :test test))))     ;fuck. too stupid. exchange args' position!
+;; general variables 
+(defparameter +operators+ (vector '+ '- '* '/ '^))
+(defparameter *op-predicts* nil)
 
 
+;; predicts about operator
+(defmacro defoperator-p (name &key (start 0) end)
+  `(defun ,name (obj)
+     (find obj +operators+ :start ,start :end ,end))
+  ;`(push #',name *op-predicts*)
+  )
+
+(defoperator-p operatorp)
+(defoperator-p junior-operator-p :start 0 :end 2)
+(defoperator-p midddle-operator-p :start 2 :end 4)
+(defoperator-p senior-operator-p :start 4 :end 5)
+
+
+;; supply functions
+(defun translated-form-p (expr)
+  (operatorp (car expr)))
+
+(defun single-elt-p (expr)
+  (eql (cdr expr) nil))
+
+
+;; main convert function
+(defun loop-per-operator (form fn)
+  (labels ((convert-form (form)
+             (cond ((or (atom form)
+                        (translated-form-p form))
+                    form)
+                   ((single-elt-p form) (convert-form (car form)))
+                   (t (destructuring-bind (fst operator sec &rest rest) form
+                        (if (funcall fn operator)
+                            (convert-form `((,operator
+                                             ,(convert-form fst)
+                                             ,(convert-form sec))
+                                            ,@rest))
+                            `(,(convert-form fst)
+                              ,operator
+                              ,(convert-form `(,sec ,@rest)))))))))
+    (convert-form form)))
+
+;; front
+(defmacro eval-expr (form)
+  ;(mapcar #'(lambda (fn) (loop-per-operator form fn)) *op-predicts*)
+  (labels ((iter (form predicts-lst)
+             (if (null predicts-lst)
+                 form
+                 (iter (loop-per-operator form (car predicts-lst)) (cdr predicts-lst)))))
+    (iter form *op-predicts*)))
